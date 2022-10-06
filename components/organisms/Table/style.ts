4ec473d17b2b0a2e20b0b761e2ee9a3a3af5238d
LeeSongA <thddk0317@gmail.com>
2022-10-06 17:52:37 +0900

    table을 grid로 작성

--- components/organisms/Table/style.ts
+++ components/organisms/Table/style.ts
@@ -4,20 +4,18 @@ const S = {
   Table: styled.table`
     width: 100%;
     border-collapse: collapse;
+    display: grid;
+    grid-template-columns: 1fr 2fr 1fr 1fr 1fr 1fr;
   `,
   TableHead: styled.tr`
     height: 40px;
     line-height: 40px;
     border-bottom: 1px solid #000000;
-    display: grid;
-    grid-template-columns: 200px 1fr 25% 200px 1fr 30%;
   `,
   TableRow: styled.tr`
     height: 40px;
     line-height: 40px;
-    border-bottom: 1px solid #000000;   
-    display: grid;
-    grid-template-columns: 200px 1fr 25% 200px 1fr 30%;
+    border-bottom: 1px solid #000000;
 
     &:last-child {
       border-bottom: none;
