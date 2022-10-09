005b36f2ae64f20d685cc3384aa1798e694c9db1
LeeSongA <thddk0317@gmail.com>
2022-10-09 12:46:44 +0900

    페이지에 CheckableGridTable 추가

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,16 @@
 import GridTable from "../components/organisms/GridTable";
+import CheckableGridTable from "../components/organisms/CheckableGridTable";
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <GridTable />
+  return (
+    <>
+      <h1>Table</h1>
+      <GridTable />
+      <h1>CheckableTable</h1>
+      <CheckableGridTable />
+    </>
+  ) 
 }
 
 export default MyApp
