e48bf124ff501f1d9c6a90bed0bf734b73a95658
LeeSongA <thddk0317@gmail.com>
2022-10-09 10:33:36 +0900

    :pencil2: table을 grid layout으로 작성하고 스타일 추가

--- components/organisms/GridTable/style.ts
+++ components/organisms/GridTable/style.ts
@@ -2,7 +2,17 @@ import styled from "styled-components";
 
 const S = {
   Wrapper: styled.table`
+    width: 100%;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+
+    th {
+      border-bottom: 1px solid #000000;
+    }
+    td {
+      border-bottom: 1px solid #000000;
+    }
   `
-} 
+}
 
 export default S;
